894805aa109668f823d381975543595f0e249d3c
William Woodruff <william@tuffbizz.com>
2015-12-03 01:05:14 -0500

    begin to fill in arrays

---
+++ genregen.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+
+ADJECTIVES = [
+	"smooth",
+	"ambient",
+	"hardcore",
+	"indie",
+	"industrial",
+	"alternative",
+	"progressive",
+	"instrumental",
+	"new-age",
+	"cosmic",
+	"live",
+	"improvised",
+	"new",
+	"old-school",
+	"hard",
+	"east-coast",
+	"west-coast",
+	"midwest",
+	"continental",
+	"international",
+	"twee",
+	"christian",
+	"heavy",
+	"epic",
+	"digital",
+	"street",
+	"ghetto",
+	"hood",
+	"freaky",
+	"epic",
+	"electronic",
+	"soft",
+	"art",
+	"garage",
+	"melodic",
+	"surf"
+]
+
+PREFIXES = [
+	"neo",
+	"post",
+	"proto",
+	"wave",
+	"vapor",
+	"trance",
+	"K",
+	"J",
+	"dub",
+	"drone",
+	"hard",
+	"electro",
+	"nu",
+	"synth",
+	"anti",
+	"acid",
+	"freak",
+	"ethno",
+	"cow",
+	"doom",
+	"happy",
+	"sad",
+	"crust",
+	"kraut",
+	"anarcho",
+	"thrash",
+	"power",
+	"riot",
+	"funk",
+	"metal"
+]
+
+SUFFIXES = [
+	"core",
+	"style",
+	"wave",
+	"beat",
+	"punk",
+	"tech",
+	"funk",
+	"clash",
+	"pop",
+	"grind",
+	"violence"
+]
+
+GENRES = [
+
+]

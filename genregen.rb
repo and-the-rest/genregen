c89270a0e07d41c6c36e37a3d34026e7877bd1fe
William Woodruff <william@tuffbizz.com>
2015-12-06 19:03:04 -0500

    stub templating out

--- genregen.rb
+++ genregen.rb
@@ -5,7 +5,35 @@ require 'yaml'
 module GenreGen
 	DATA = YAML::load_file('data.yml')
 
-	def self.genre
+	def self.generate
+		template = DATA['templates'].sample
+		fill_template(template)
+	end
 
+	def self.fill_template(template)
+		template % {
+			adjective: adjective,
+			genre: genre,
+			prefix: prefix,
+			suffix: suffix
+		}
+	end
+
+	def self.adjective
+		'adjective'
+	end
+
+	def self.genre
+		'genre'
+	end
+
+	def self.prefix
+		'prefix'
+	end
+
+	def self.suffix
+		'suffix'
 	end
 end
+
+puts GenreGen.generate
